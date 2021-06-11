645202d53bc30633d5969ba76ecbc70882102dbc
Ambar Khare <ambar.khare24@gmail.com>
2021-06-11 18:47:09 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,10 @@
 # Build your say_hello method here
 
+#say_hello method (greets the user) defined with a default arguement
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
 end
+
+say_hello(Ambar)
+
+say_hello
